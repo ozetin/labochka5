b67ca728a2c9f7deab95428ade6d1f4f60fe5610
student-09-525 <student-09-525@ncfu.net>
2025-12-03 12:31:37 +0300

    изменено решение задания 2

--- Задания/zadanie2.py
+++ Задания/zadanie2.py
@@ -6,9 +6,14 @@ import sys
 if __name__ == '__main__':
 
     marshruti = []
-    print("add - добавить маршрут")
-    print("end - закончить ввод маршрутов")
     while True:
+
+        print("Список команд:\n")
+        print("add - добавить маршрут")
+        print("list - вывести список маршрутов")
+        print("isk - поиск маршрута по номеру")
+        print("end - завершить работу программы")
+
         a = input().lower()
 
         if a == "add":
@@ -25,19 +30,30 @@ if __name__ == '__main__':
 
             if marshruti:
                 marshruti.sort(key=lambda x: x.get("number",""))
+
+        elif a == "list":
+            for i in marshruti:
+                print("Начальный маршрут: ", i["start"])
+                print("Конечный маршрут: ", i["end"])
+                print("Номер маршрута: ", i["number"], "\n")
+
+        elif a == "isk":
+            isk_numb = int(input("Введите номер искомого маршрута: "))
+            schet = 0
+
+            for i in marshruti:
+                if i["number"] == isk_numb:
+                    schet += 1
+                    print(f"Начальный пункт: {i['start']}")
+                    print(f"Конечный пункт: {i['end']}")
+                    print(f"Номер маршрута: {i['number']}")
+            if schet == 0:
+                print("Таких маршрутов нет", file=sys.stderr)
+
         elif a == "end":
             break
+
         else:
-            print("неизвестная команда")
+            print("Неизвестная команда")
 
-    isk_numb = int(input("Введите номер искомого маршрута: "))
-    schet = 0
 
-    for i in marshruti:
-        if i["number"] == isk_numb:
-            schet += 1
-            print(f"Начальный пункт: {i['start']}")
-            print(f"Конечный пункт: {i['end']}")
-            print(f"Номер маршрута: {i['number']}")
-    if schet == 0:
-        print("Таких маршрутов нет", file=sys.stderr)
